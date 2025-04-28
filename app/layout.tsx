d70faa8f0eb72462c58c33ca3d36db74be1f652e
Taine Buchan <tainebuchanwork@gmail.com>
2025-04-28 22:13:16 +1200

    feat: on Success toaster complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import './globals.css'
 import { ReactNode } from 'react'
+import { Toaster, toast } from 'sonner'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="mx-auto max-w-[960px] px-4 md:px-6 lg:px-8">
       <body>
+        <Toaster />
         <Navbar />
         {children}
         <Footer />
