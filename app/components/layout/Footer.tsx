c1fa08a0a5aeb3e1d2559587dea94048ddc80fa6
Taine Buchan <tainebuchanwork@gmail.com>
2025-04-28 21:22:42 +1200

    feat: footer

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
-const Footer = () => {
-  return <div>Footer</div>
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-6">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+        <p className="text-gray-300">
+          © {new Date().getFullYear()} Water Blasting NZ. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
 }
-
-export default Footer
